9bc9771d0848e5fbeb461d32ce043df731abebd7
DenizUgur <deniz343@gmail.com>
2020-06-19 18:37:09 +0300

    react-ui branch init

--- build/build.js
+++
@@ -1,9 +0,0 @@
-const webpack = require("webpack");
-const config = require("./webpack.app.config");
-
-const env = "production";
-const compiler = webpack(config(env));
-
-compiler.run((err, stats) => {
-    return err;
-});
