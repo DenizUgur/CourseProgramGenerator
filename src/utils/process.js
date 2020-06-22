a831e17ccb96d69b782e09b16ba76877f180b927
DenizUgur <deniz343@gmail.com>
2020-06-23 01:08:51 +0300

    removed getWorld and adjusted tests

--- src/utils/process.js
+++ src/utils/process.js
@@ -1,79 +1,6 @@
 /* eslint-disable no-undef */
-const XLSX = require("xlsx");
 const moment = require("moment");
-const path = require("path");
-const fs = require("fs");
-
-let directoryPath = path.join(__dirname, "bin");
-var worksheet = null;
 var errors = [];
-
-export function getWorld() {
-  var world = [];
-  return new Promise((resolve, reject) => {
-    if (process.env.NODE_ENV === 'test') {
-      directoryPath = path.join(__dirname, "../../__mocks__/test_catalog");
-    }
-    fs.readdir(directoryPath, function(err, files) {
-      if (err) {
-        log("Catalogs must be placed in 'bin' folder");
-        reject();
-      }
-
-      files.forEach(function(file) {
-        if (file === ".DS_Store") return false;
-        var workbook = XLSX.readFile(path.join(directoryPath, file));
-        var data = workbook.SheetNames[0];
-        worksheet = workbook.Sheets[data];
-
-        for (var ii = 2; cell(0, ii) !== undefined; ii++) {
-          var course = {
-            name: null,
-            credits: null,
-            class: null,
-            teacher: null,
-            corequisite: null,
-            hours: []
-          };
-          course.name = cell(0, ii) + "." + cell(1, ii);
-          course.credits = parseInt(cell(5, ii));
-          course.class = cell(2, ii);
-          course.teacher = lowerCaseAllWordsExceptFirstLetters(cell(3, ii));
-
-          //corequisite
-          if (cell(4, ii) && cell(4, ii) !== "-") {
-            course.corequisite = [];
-            if (cell(4, ii).indexOf(" ve ") !== -1) {
-              course.corequisite = cell(4, ii).split(" ve ");
-            } else {
-              course.corequisite = cell(4, ii).split(" and ");
-            }
-            course.corequisite = course.corequisite.map(item => {
-              return item.replace(" ", ".");
-            });
-          }
-
-          //hours
-          var hours = cell(6, ii).split("\n");
-          for (var hour in hours) {
-            if (hours[hour] === "") continue;
-            try {
-              var hourOBJ = parseHourOBJ(hours[hour]);
-              if (
-                course.hours.length > 0 &&
-                isEqual(hourOBJ, course.hours[course.hours.length - 1])
-              )
-                continue;
-              course.hours.push(hourOBJ);
-            } catch (ex) {}
-          }
-          world.push(course);
-        }
-      });
-      resolve(world);
-    });
-  });
-}
 
 export function getResult(world, input, unavailable_hours = "") {
   return new Promise((resolve, reject) => {
@@ -415,48 +342,9 @@ function isColliding(a, b) {
   return (a[0] <= b[0] && b[0] <= a[1]) || (b[0] <= a[0] && a[0] <= b[1]);
 }
 
-function lowerCaseAllWordsExceptFirstLetters(input) {
-  var arr = input.split(" ");
-  arr.forEach(function(word, i) {
-    arr[i] = word.charAt(0) + word.slice(1).toLowerCase();
-  });
-  return arr.join(" ");
-};
-
-function cell(i, ii) {
-  var read_cell = ["A", "B", "C", "G", "I", "F", "L"];
-  var desired_cell = worksheet[read_cell[i] + ii];
-  return desired_cell ? desired_cell.v : undefined;
-}
-
 function isEqual(a, b) {
   if (a === undefined || b === undefined) return false;
   return a[0] === b[0] && a[1] === b[1];
-}
-
-function parseHourOBJ(origin) {
-  moment.locale("tr");
-  var hourOBJ = [null, null];
-
-  var day = origin.split(" | ")[0];
-  hourOBJ[0] = origin
-    .split(" | ")[1]
-    .split(" - ")[0]
-    .trim();
-  hourOBJ[1] = origin
-    .split(" | ")[1]
-    .split(" - ")[1]
-    .trim();
-
-  day = moment.weekdays(true).indexOf(day);
-  hourOBJ[0] = moment("1," + hourOBJ[0], "d,HH:mm")
-    .add(day, "d")
-    .valueOf();
-  hourOBJ[1] = moment("1," + hourOBJ[1], "d,HH:mm")
-    .add(day, "d")
-    .valueOf();
-
-  return hourOBJ;
 }
 
 function log(msg) {
