f90d93a1170aec62f81acba99cb876785d1bfbf8
DenizUgur <deniz343@gmail.com>
2020-06-25 02:02:24 +0300

    update

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,7 @@
-import Event from './Event/Event';
+import Event, { EmptyEvent } from './Event/Event';
 import Schedule from './Schedule/Schedule';
 import Controls from './Controls/Controls';
 import WelcomeScreen from './WelcomeScreen/WelcomeScreen';
 import NoWiFi from './NoWiFi/NoWiFi';
 
-export { Event, Schedule, Controls, WelcomeScreen, NoWiFi };
+export { Event, EmptyEvent, Schedule, Controls, WelcomeScreen, NoWiFi };
